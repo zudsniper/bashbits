a8c7bdf50f8123065721f9a1eada9a8bd2a39b82
zod <me@zod.tf>
2023-06-09 04:00:22 -0500

    d

---
+++ builders/build_tsnode.sh
@@ -0,0 +1,129 @@
+#!/bin/bash
+# build_tsnode.sh
+# ---------------
+# 
+# Take your current typescript + nodejs project and complete the construction of a git repository and key files. 
+# 🔴 NOT ERROR HANDLED YET
+#
+# @zudsniper
+
+# Source the ANSI color codes
+source ~/.ansi_colors.sh
+
+# Function to log messages
+log() {
+  printf "\n${ANSI_GREEN}$1${ANSI_RESET}\n"
+}
+
+# Get the project name
+read -p "📝 Enter the name of your project: " project_name
+
+# Check if the project directory already exists
+if [ -d "$project_name" ]; then
+  log "⚠️  The directory $project_name already exists. Checking the state of the project..."
+
+  cd $project_name
+
+  # Check for package.json, tsconfig.json, and src/index.ts
+  if [ -f "package.json" ] && [ -f "tsconfig.json" ] && [ -f "src/index.ts" ]; then
+    log "✅  Found package.json, tsconfig.json, and src/index.ts."
+
+    # Check for a Git repository
+    if [ -d ".git" ]; then
+      log "✅  Found a Git repository."
+
+      # Check the remote origin
+      git_remote_origin=$(git config --get remote.origin.url)
+      if [ -n "$git_remote_origin" ]; then
+        log "✅  Found a remote origin: $git_remote_origin"
+      else
+        log "❌  No remote origin found."
+      fi
+    else
+      log "❌  No Git repository found."
+    fi
+
+    # Ask if the user wants to run and test the program
+    read -p "❓ Do you want to run and test the program? (y/n): " run_and_test
+    if [ "$run_and_test" = "y" ]; then
+      # Install dependencies
+      log "📦 Installing dependencies..."
+      npm install
+
+      # Build the program
+      log "🔨 Building the program..."
+      npx tsc
+
+      # Run the program
+      log "🚀 Running the program..."
+      node dist/index.js
+    fi
+  else
+    log "❌  The necessary files for a TypeScript + Node.js project are not found."
+  fi
+
+  # Ask if the user wants to continue
+  read -p "❓ Do you want to continue? (y/n): " continue
+  if [ "$continue" != "y" ]; then
+    exit 1
+  fi
+else
+  # Create the project directory
+  log "📂 Creating the project directory..."
+  mkdir $project_name
+  cd $project_name
+fi
+
+# Initialize a new Node.js project
+log "📦 Initializing a new Node.js project..."
+npm init -y
+
+# Install TypeScript and ts-node
+log "📦 Installing TypeScript and ts-node..."
+npm install --save-dev typescript ts-node
+
+# Create a tsconfig.json file
+log "📝 Creating a tsconfig.json file..."
+cat <<EOF > tsconfig.json
+{
+  "compilerOptions": {
+    "target": "es5",
+    "module": "commonjs",
+    "strict": true,
+    "esModuleInterop": true,
+    "outDir": "dist"
+  },
+  "include": ["src"],
+  "exclude": ["node_modules"]
+}
+EOF
+
+# Create the source directory and a sample TypeScript file
+log "📂 Creating the source directory and a sample TypeScript file..."
+mkdir src
+echo 'console.log("Hello, world!");' > src/index.ts
+
+# Initialize a new Git repository
+log "🔨 Initializinga new Git repository..."
+git init
+git add .
+git commit -m "🎉 Initial commit"
+
+# Ask if the user wants to push changes to the remote origin
+if [ -n "$git_remote_origin" ]; then
+  read -p "❓ Do you want to push changes to the remote origin? (y/n): " push_changes
+  if [ "$push_changes" = "y" ]; then
+    git push -f origin master
+  fi
+fi
+
+log "🎉 Done!"
+
+# Print a cute little bunny rabbit
+echo -e "${ANSI_GREEN}"
+cat << "EOF"
+ (\(\ 
+ ( -.-)
+ o_(")(")
+EOF
+echo -e "${ANSI_RESET}"
---
+++ builders/deploy_ts_pkg.sh
@@ -0,0 +1,94 @@
+#!/bin/bash
+# deploy_pkg.sh
+# -------------
+# 
+# Deploy a nodeJS TypeScript package to 1. aptitude and/or 2. homebrew. 
+# 🔴 NOT ERROR HANDLED YET
+#
+# @zudsniper
+
+# Source the ANSI color codes
+source ~/.ansi_colors.sh
+
+# Define the progress icon
+progress_icon=("🕐" "🕑" "🕒" "🕓" "🕔" "🕕" "🕖" "🕗" "🕘" "🕙" "🕚" "🕛")
+progress_icon_index=0
+
+# Function to show the progress icon
+show_progress_icon() {
+  printf "\r${progress_icon[$progress_icon_index]}"
+  progress_icon_index=$(( (progress_icon_index + 1) % ${#progress_icon[@]} ))
+}
+
+# Function to log messages
+log() {
+  printf "\n${ANSI_GREEN}$1${ANSI_RESET}\n"
+}
+
+# Install nexe
+log "📦 Installing nexe..."
+npm install -g nexe
+show_progress_icon
+
+# Compile TypeScript to JavaScript
+log "🔨 Compiling TypeScript to JavaScript..."
+npx tsc
+show_progress_icon
+
+# Package the application
+log "📦 Packaging the application..."
+read -p "Enter the entry point of your application (e.g., dist/index.js): " entry_point
+read -p "Enter the output name for your executable: " output_name
+nexe $entry_point -o $output_name
+show_progress_icon
+
+# Publish to GitHub Packages
+log "🚀 Publishing to GitHub Packages..."
+read -p "Enter your GitHub username: " github_username
+read -s -p "Enter your GitHub token: " github_token
+read -p "Enter the name of your package: " package_name
+
+echo "//npm.pkg.github.com/:_authToken=$github_token" > .npmrc
+echo "@$github_username:registry=https://npm.pkg.github.com" >> .npmrc
+
+npm publish
+show_progress_icon
+
+# Ask if the user wants to create a Homebrew formula
+read -p "❓Generate Homebrew Formula? (y/n): " generate_homebrew_formula
+if [ "$generate_homebrew_formula" = "y" ]; then
+  log "🍺 Creating a Homebrew formula..."
+  read -p "Enter the URL of the tarball of your application: " tarball_url
+  read -p "Enter the SHA256 checksum of the tarball: " sha256_checksum
+  read -p "Enter the description of your application: " description
+  read -p "Enter the homepage of your application: " homepage
+
+  cat <<EOF > $output_name.rb
+  class $(echo $output_name | tr '-' '_') < Formula
+    desc "$description"
+    homepage "$homepage"
+    url "$tarball_url"
+    sha256 "$sha256_checksum"
+
+    depends_on "node"
+
+    def install
+      bin.install "$output_name"
+    end
+  end
+  EOF
+  show_progress_icon
+
+  # Push the Homebrew formula to your tap repository
+  log "🚀 Pushing the Homebrew formula to your tap repository..."
+  read -p "Enter the path to your local clone of your tap repository: " tap_path
+
+  cp $output_name.rb $tap_path/Formula/
+  cd $tap_path
+  git add .
+  git commit -m "Add $output_name formula"
+  git push
+  show_progress_icon
+fi
+
+log "🎉 Done!"
