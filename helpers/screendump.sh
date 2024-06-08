5f9875954e3fae68fad52d2113ff87fb1ce6e1c4
zod <me@zod.tf>
2024-06-08 02:56:15 -0500

    screendump.sh v0.1.0

--- helpers/screendump.sh
+++ helpers/screendump.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# screendump.sh v0.0.1
+# screendump.sh v0.1.0
 #
 # creates alias 'screendump' to post all terminal output to termbin.com
 # BE CAREFUL THIS WILL EXPOSE ANY SENSITIVE INFORMATION THAT IS WRITTEN TO TERMINAL IN PLAIN TEXT!!
@@ -7,4 +7,4 @@
 # $ source screendump.sh
 # $ screendump 
 # 
-alias screendump='which screen &>/dev/null || sudo apt-get install -y screen; which nc &>/dev/null || sudo apt-get install -y netcat; screen -L -dm bash -c "cat /dev/stdout | nc termbin.com 9999"'
+alias screendump='which screen &>/dev/null || sudo apt-get install -y screen; which nc &>/dev/null || sudo apt-get install -y netcat; screen -L -dm bash -c "cat /dev/stdout | nc termbin.com 9999" | tee /tmp/termbin_link && echo \"Termbin link: $(cat /tmp/termbin_link)\"'
